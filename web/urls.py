6274715cd1cf90f2927f74560ef5b1841127bbb2
Miguel <tecnologia1@zcmayoristas.com>
2024-02-27 15:33:55 -0500

    modulo 2

--- web/urls.py
+++ web/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
 
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
+app_name = 'web'
 urlpatterns = [
-    path('',include('web.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index,name='index'),
+    path('productosPorCategoria/<int:categoria_id>',views.productosPorCategorias,name='productosPorCategoria'),
+    path('productosPorNombre',views.productosPorNombre,name='productosPorNombre'),
+    path('producto/<int:producto_id>',views.productoDetalle,name='producto'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
